81aaaca7ed85779bacdceca41ba638889570b004
aeisenba61 <aeisenba@gmail.com>
2018-03-17 12:03:07 -0400

    update #25mil

--- SQL/create DATA_MCDS table.sql
+++ SQL/create DATA_MCDS table.sql
@@ -1,31 +1,24 @@
-create table DATA_MCDS as 
+create table DATA_MCDS as
 select 
-	 p.fips
-	,p.stname
-	,p.ctyname
-	,p.abbrev
-	,p.popestimate2016
-	,case when o.obesity_per is null or o.obesity_per = 0 then 0 else o.obesity_n/(o.obesity_per/100) end as obesity_pop
-	,o.obesity_n
-	,o.obesity_per
-	,case when o.diab_per is null or o.diab_per = 0 then 0 else o.diab_n/(o.diab_per/100) end as diab_pop
-	,o.diab_n
-	,o.diab_per
-	,f.pop2010
-	,f.ohu2010
-	,f.medianfamilyincome
-	,f.lapop20
-	,f.lalowi20
-	,f.lasnap20
-	,f.tractlowi
-	,f.tractsnap
-	,f.lapop20_share
-	,f.lalowi20_share
-	,f.lasnap20_share
-	,m.mccount
-	,case when m.mccount is null or p.popestimate2016 = 0 then 0 else m.mccount/p.popestimate2016 end as mcd_per_cap
---	,PRIMARY KEY(`fips`)
-from population p 
-	left join obeseDiabetes o on p.fips = o.fips
-	left join foodDesert f on p.fips = f.fips
-	left join countyMcdsCount m on p.fips = m.county_fips;
+ p.fips
+,p.stname
+,p.ctyname
+,p.abbrev
+,p.popestimate2016
+,case when o.obesity_per is null or o.obesity_per = 0 then 0 else o.obesity_n/(o.obesity_per/100) end as obesity_pop
+,o.obesity_n
+,o.obesity_per
+,case when o.diab_per is null or o.diab_per = 0 then 0 else o.diab_n/(o.diab_per/100) end as diab_pop
+,o.diab_n
+,o.diab_per
+,f.pop2010
+,f.ohu2010
+,f.medianfamilyincome
+,f.lapop20
+,f.lalowi20, f.lasnap20, f.tractlowi, f.tractsnap, f.lapop20_share, f.lalowi20_share, f.lasnap20_share, m.mccount
+,case when m.mccount is null or p.popestimate2016 = 0 then 0 else m.mccount/p.popestimate2016 end as mcd_per_cap
+,case when p.popEstimate2016 = 0 or p.popEstimate2016 is null then 0 else f.tractlowi/p.popEstimate2016*100 end as low_inc_pop_per 
+,case when f.ohu2010 = 0 or f.ohu2010 is null then 0 else f.tractsnap/f.ohu2010*100 end as snap_households_per 
+from population p left join obeseDiabetes o on p.fips = o.fips 
+left join foodDesert f on p.fips = f.fips 
+left join countyMcdsCount m on p.fips = m.county_fips;`
--- SQL/create state level.sql
+++ SQL/create state level.sql
@@ -1,7 +1,7 @@
 --create table stateLevelMc as
 SELECT
 stName
-,Case WHEN stName = 'District of Columbia' then 'DC' else Abbrev end as abbrev
+,Case WHEN stName = 'District of Columbia' then 'DC' else Abbrev end as Abbrev
 	,sum(popEstimate2016) as pop2016
 	,sum(obesity_n) as obesity_n
 	,sum(obesity_n) / sum(obesity_pop) * 100 as stObesityPer
@@ -19,7 +19,7 @@ stName
 	,avg(lalowi20_share) as lalowi20_share
 	,avg(lasnap20_share) as lasnap20_share
 	,sum(mccount)/sum(popEstimate2016) as mcd_per_cap
-	,sum(tractLOWI) / sum(popEstimate2016) *100 as lowipopst_low_inc_pop_per
+	,sum(tractLOWI) / sum(popEstimate2016) *100 as st_low_inc_pop_per
 	,sum(tractSNAP) / sum(ohu2010) *100 as st_snap_households_per
 	,sum(mccount) as mcCount	
 FROM DATA_MCDS
