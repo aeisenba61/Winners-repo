19d6de5afcd8ec356b75ed4e0a762fc1f80a357d
aeisenba61 <aeisenba@gmail.com>
2018-03-17 09:03:43 -0400

    State Summary fix 2

--- SQL/create state level.sql
+++ SQL/create state level.sql
@@ -4,9 +4,9 @@ SELECT distinct
 	,Case WHEN stName = 'District of Columbia' then 'DC' else Abbrev end as Abbrev
 	,sum(popEstimate2016) as pop2016
 	,sum(obesity_n) as obesity_n
-	,sum(obesity_n) / sum(obesity_pop) * 100 as obesity_%
+	,sum(obesity_n) / sum(obesity_pop) * 100 as stObesityPer
 	,sum(diab_n) as diab_n
-	,sum(diab_n) / sum(diab_pop)  * 100 as diab_per	
+	,sum(diab_n) / sum(diab_pop)  * 100 as stDiabPer
 	,sum(pop2010) as statePop2010	
 	,sum(ohu2010) as housingUnits2010	
 	,sum(medianFamilyIncome) as medFamIncome
@@ -15,9 +15,9 @@ SELECT distinct
 	,sum(lasnap20) as lasnap20
 	,sum(tractLOWI) as tractLOWI
 	,sum(tractSNAP) as tractSNAP
-	,sum(lapop20_share) as lapop20_share
-	,sum(lalowi20_share) as lalowi20_share
-	,sum(lasnap20_share) as lasnap20_share
+	,avg(lapop20_share) as lapop20_share
+	,avg(lalowi20_share) as lalowi20_share
+	,avg(lasnap20_share) as lasnap20_share
 	,sum(mccount)/sum(popEstimate2016) as mcd_per_cap
 	,sum(mccount) as mcCount
 	,PRIMARY KEY(`stName`)
