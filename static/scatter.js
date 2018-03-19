cd6522be14a52491d6a4bb94f4c9bb9db57a85ec
Lee Hughes <lev.hughes09@gmail.com>
2018-03-19 07:16:59 -0400

    latest scatter

--- static/scatter.js
+++ static/scatter.js
@@ -1,122 +1,172 @@
-var svgWidth = 960;
-var svgHeight = 500;
+// Responsive
+d3.select(window).on("resize", resize);
 
-var margin = { top: 20, right: 40, bottom: 60, left: 100 };
+function resize() {
+  var svgArea = d3.select("svg");
 
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
+  if (!svgArea.empty()) {
+    svgArea.remove();
+    renderScatter("diabetes");
+  }
+}
 
-// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select("#scatter")
-			.append("svg")
-			.attr("width", svgWidth)
-			.attr("height", svgHeight)
-			.append("g")
-			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+// Render default chart
+renderScatter("diabetes");
 
-var chart = svg.append("g");
-var dataPoint = "% diabetic adults"//document.getElementById("#selDataset").value
-console.log(dataPoint)
+function renderScatter(selected) {
 
-// Append a div to the body to create tooltips, assign it a class
-d3.select("#scatter")
-  .append("div")
-  .attr("class", "tooltip")
-  .style("opacity", 0); 
- 
-d3.json("http://localhost:5000/state_info", function(err, stData) {
-  if (err) throw err;
-  console.log(stData);
-  console.log(Object.keys(stData).length);
-  var dataAttr = [];
-  var dataMcd = []
+// Incorporate selected variable
+
+  var var_n = `${selected}_n`;
+  var selected = `${selected}`;
+
+    var all_labels = {diabetes: "diabetic",
+                      obesity: "obese",
+                      pov: "low income",
+                      snap: "receiving SNAP"};
   
-  for(var prop in stData){
-	var state = stData[prop]
-	dataMcd.push(state.mcd_per_cap);
-	dataAttr.push(state.diabetes);
-    /*switch (dataPoint){
-		case "% diabetic adults":
-			dataAttr.push(+state.diabetes)
-		case "% obese adults":
-			dataAttr.push(+state.obesity)
-		case "% households receiving SNAP benefits":
-			dataAttr.push(+state.snap)
-		case "% low income population":
-			dataAttr.push(+state.pov)
-		default:
-			dataAttr.push(+state.diabetes)
-
-	}*/
-  };
   
-  console.log(dataAttr)
-  console.log(dataMcd)
-  // Create scale functions
-  var yLinearScale = d3.scaleLinear()
-    .range([height, 0]);
 
-  var xLinearScale = d3.scaleLinear()
-    .range([0, width]);
 
-  // Create axis functions
-  var bottomAxis = d3.axisBottom(xLinearScale);
-  var leftAxis = d3.axisLeft(yLinearScale);
+// Scatter stuff
 
-  // Scale the domain
-  xLinearScale.domain([0, d3.max(dataMcd)]);
-  yLinearScale.domain([0, d3.max(dataAttr)]);
+  var svgWidth = window.innerWidth;
+  var svgHeight = window.innerHeight;
 
-  var toolTip = d3.tip()
+  var margin = { top: 20, right: 60, bottom: 85, left: 50 };
+
+  var width = Math.min(1032, svgWidth - margin.left - margin.right);
+  var height = svgHeight - margin.top - margin.bottom;
+
+  // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+  var svg = d3.select("#scatter")
+  			.append("svg")
+  			.attr("width", svgWidth)
+  			.attr("height", svgHeight)
+  			.append("g")
+  			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var chart = svg.append("g");
+  var dataPoint = `% ${all_labels[selected]}`
+
+
+  // Append a div to the body to create tooltips, assign it a class
+  d3.select("#scatter")
+    .append("div")
     .attr("class", "tooltip")
-    .offset([-25, -10])
-    .html(function(data,index) {
-      var stAbbrev = stData[index].state_abbrev;
-      var mcdCount = +stData[index].mccount;
-      return (stAbbrev + "<hr>McDonalds Count: " + mcdCount + "<br> "+ dataPoint +": " + dataAttr[index]);
-    });
+    .style("opacity", 0);
 
-  chart.call(toolTip);
+  d3.json("/state_info", function(err, stData) {
 
-  chart.selectAll("circle")
-    .data(dataMcd)
-    .enter().append("circle")
-      .attr("cx", function(data, index) {
-        return xLinearScale(dataMcd[index]);
-      })
-      .attr("cy", function(data, index) {
-        return yLinearScale(dataAttr[index]);
-      })
-      .attr("r", "10")
-      .attr("fill", "#dd1021")
-	  .attr("stroke","#ffc300")
-	  .attr("opacity","0.8")
-      .on("click", function(data, index) {
-        toolTip.show(data);
-      })
-      // onmouseout event
-      .on("mouseout", function(data, index) {
-        toolTip.hide(data);
+    if (err) throw err;
+
+	var dataAttr = [];
+    var dataMcd = [];
+    var dataAbbr = [];
+    var dataCnt = [];
+
+    for(var state in stData) {
+    	var state = stData[state];
+    	dataMcd.push(state.mcd_per_cap);
+    	dataAttr.push(state[selected]);
+		dataAbbr.push(state.state_abbrev);
+		dataCnt.push(state.mcCount);
+    };
+
+	
+    // Create scale functions
+    var yLinearScale = d3.scaleLinear()
+      .range([height, 0]);
+
+    var xLinearScale = d3.scaleLinear()
+      .range([0, width]);
+
+    // Create axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Scale the domain
+    xLinearScale.domain([d3.min(dataMcd) - .000001, d3.max(dataMcd) + .000001]);
+    yLinearScale.domain([0, d3.max(dataAttr) + 2]);
+
+    var toolTip = d3.tip()
+      .attr("class", "tooltip")
+      .offset([-25, -40])
+      .html(function(d,i) {
+        var stAbbrev = dataAbbr[i];
+        var mcdCount = +dataCnt[i];
+		var stAttr = +dataAttr[i]
+        return (stAbbrev + "<hr>McDonalds Count: " + mcdCount + "<br> "+ dataPoint +": " + stAttr);
       });
 
-  chart.append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(bottomAxis);
+    chart.call(toolTip);
 
-  chart.append("g")
-    .call(leftAxis);
+    chart.selectAll("circle")
+      .data(dataMcd)
+      .enter().append("circle")
+        .attr("cx", function(data, index) {
+          return xLinearScale(dataMcd[index]);
+        })
+        .attr("cy", function(data, index) {
+          return yLinearScale(dataAttr[index]);
+        })
+        .attr("r", "10")
+        .attr("fill", "#dd1021")
+		.attr("stroke","#ffc300")
+		.attr("opacity","0.8")
+		.on("click", function(d) {
+          toolTip.show(d);
+        })
+        // onmouseout event
+        .on("mouseout", function(d) {
+          toolTip.hide(d);
+        });
 
-  chart.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left + 40)
-      .attr("x", 0 - (height / 2))
-      .attr("dy", "1em")
+    chart.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis)
+        .selectAll("text")
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", ".15em")
+            .attr("transform", function(d) {
+                return "rotate(-30)"
+                });;
+
+    chart.append("g")
+      .call(leftAxis);
+
+    chart.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text(dataPoint);
+
+  // Append x-axis labels
+    chart.append("text")
+      .attr("transform", "translate(" + (width / 2) + " ," + (height + margin.top + 50) + ")")
       .attr("class", "axisText")
-      .text(dataPoint);
+      .text("McDonald's Per Capita");
+  });
 
-// Append x-axis labels
-  chart.append("text")
-    .attr("transform", "translate(" + (width / 2) + " ," + (height + margin.top + 30) + ")")
-    .attr("class", "axisText")
-    .text("McDonald's Per Capita");
-});
+}
+
+
+
+/////////////////////
+// Update
+/////////////////////
+
+function optionChanged(new_var) {
+
+// Delete old
+    var svgArea = d3.selectAll("svg");
+    if (!svgArea.empty()) {
+        svgArea.remove();
+    }
+
+// Rerun
+    renderScatter(new_var);
+}
