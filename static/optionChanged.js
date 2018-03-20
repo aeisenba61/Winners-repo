96825f00ed417c878b4688ec12bbd21f639792c7
Xin Yuen <cxinya@gmail.com>
2018-03-20 08:02:43 -0400

    change choropleth optionChange

--- static/optionChanged.js
+++ static/optionChanged.js
@@ -6,18 +6,9 @@ function optionChanged(new_var) {
         svgArea.remove();
     }
 
-// Rerun donut and scatter
+// Rerun
     donut(new_var);
     renderScatter(new_var);
-
-// Rerun choropleth
-    choro_vars = {
-        diabetes: "stDiabPer",
-        obesity: "stObesityPer",
-        pov: "st_low_inc_pop_per",
-        snap: "st_snap_households_per"
-    };
-
-    choropleth(choro_vars[new_var]);
+    choropleth(new_var);
 
 }
